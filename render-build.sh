59f8661256ac50321ff180bb87d0dfbbd2d89968
ChaoprayaSoft <tiawongsombat@gmail.com>
2026-05-01 17:27:01 +0700

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
+
+# Force playwright to install browsers in a local folder that Render can see
+export PLAYWRIGHT_BROWSERS_PATH=$HOME/.cache/ms-playwright
 python -m playwright install chromium
-# Note: On Render, if you hit dependency issues, you may need to use a Dockerfile.
